a041a602d16b6758a250fa19eafa1b6e6e43ede6
Abdellah El-qosby <elqosby@gmail.com>
2024-08-15 14:03:14 +0100

    font-end commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import FormComponent from "./FormComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <FormComponent />
+      </header>
     </div>
   );
 }
---
+++ src/FormComponent.js
@@ -0,0 +1,29 @@
+import "./FormComponent.css";
+import { useState } from "react";
+
+export default function FormComponent() {
+  return (
+    <div className="form-card">
+      <h2>Requesting a Loan</h2>
+      <hr />
+      <label>Name: </label>
+      <input type="text" />
+      <label>Phone number: </label>
+      <input type="text" />
+      <label>Age: </label>
+      <input type="text" />
+      <div className="employeeGroup">
+        <label>Are you an employee ? </label>
+        <input type="checkbox" id="checkbox" />
+      </div>
+      <label>Salary </label>
+      <select>
+        <option>Less than 500$</option>
+        <option>Between 500$ and 2000$</option>
+        <option>Above 2000$</option>
+      </select>
+      <hr />
+      <button id="submit-btn">Submit</button>
+    </div>
+  );
+}
